0a723a52b6419c9efcce8216d41a76372b61dad4
XxmatrixenxX <Falko116@freenet.de>
2021-10-04 01:42:18 +0200

    Added Graphs

--- Assets/LevelingScripts/TestingScript.cs
+++ Assets/LevelingScripts/TestingScript.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestingScript : MonoBehaviour
 {  
     [SerializeField] private UI_RadarChart uiStatsRadarChart;
+    public Button button;
+    private Statistics stats;
     private void Start()
     {
-       // Statistics stats = new Statistics(20);
-       //Debug.Log(stats.GetValue());
-       // uiStatsRadarChart.SetStatistic(stats);
+       stats = new Statistics(10, 40, 10,0,100);
+       uiStatsRadarChart.SetStatistic(stats);
+       Button btn = button.GetComponent<Button>();
+       btn.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick(){
+        stats.IncreaseStatAmount(Statistics.Type.Vitallity);
     }
     
 }
--- Assets/LevelingScripts/UI_RadarChart.cs
+++ Assets/LevelingScripts/UI_RadarChart.cs
@@ -1,11 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class UI_RadarChart : MonoBehaviour
 {
+  [SerializeField] private Material radarMaterial;
   private Statistics statistics;
+  private CanvasRenderer radarMeshCanvasRenderer;
+
+  private void Awake()
+  {
+    radarMeshCanvasRenderer = transform.Find("radarMesh").GetComponent<CanvasRenderer>();
+  }
 
   public void SetStatistic(Statistics statistics)
   {
@@ -20,10 +31,63 @@ public class UI_RadarChart : MonoBehaviour
 
   private void UpdateStatsVisual()
   {
-    transform.Find("VitallityBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Vitallity));
-    transform.Find("StrengthBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Strength));
-    transform.Find("CharismaBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Charisma));
-    transform.Find("AbillityBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Abillity));
-    transform.Find("AgillityBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Agillity));
+    //transform.Find("VitallityBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Vitallity));
+    //transform.Find("StrengthBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Strength));
+    //transform.Find("CharismaBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Charisma));
+    //transform.Find("AbillityBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Abillity));
+    //transform.Find("AgillityBar").localScale = new Vector3(1, statistics.GetPersentageAmount(Statistics.Type.Agillity));
+
+    Mesh mesh = new Mesh();
+
+    Vector3[] vertices = new Vector3[6];
+    Vector2[] uv = new Vector2[6];
+    int[] triangles = new int[3 * 5];
+
+    float angleIncrement = 360f / 5;
+    float RadarChartSize = 215f;
+    Vector3 vitallityVertex = Quaternion.Euler(0, 0, -angleIncrement * 0) * Vector3.up * RadarChartSize * statistics.GetPersentageAmount(Statistics.Type.Vitallity);
+    int vitallityVertexIndex = 1;
+    Vector3 abillityVertex = Quaternion.Euler(0, 0, -angleIncrement * 1) * Vector3.up * RadarChartSize * statistics.GetPersentageAmount(Statistics.Type.Abillity);
+    int abillityVertexIndex = 2;
+    Vector3 charismaVertex = Quaternion.Euler(0, 0, -angleIncrement * 2) * Vector3.up * RadarChartSize * statistics.GetPersentageAmount(Statistics.Type.Charisma);
+    int charismaVertexIndex = 3;
+    Vector3 agillityVertex = Quaternion.Euler(0, 0, -angleIncrement * 3) * Vector3.up * RadarChartSize * statistics.GetPersentageAmount(Statistics.Type.Agillity);
+    int agillityVertexIndex = 4;
+    Vector3 strengthVertex = Quaternion.Euler(0, 0, -angleIncrement * 4) * Vector3.up * RadarChartSize * statistics.GetPersentageAmount(Statistics.Type.Strength);
+    int strengthVertexIndex = 5;
+    
+    vertices[0] = Vector3.zero;
+    vertices[vitallityVertexIndex] = vitallityVertex;
+    vertices[abillityVertexIndex] = abillityVertex;
+    vertices[charismaVertexIndex] = charismaVertex;
+    vertices[agillityVertexIndex] = agillityVertex;
+    vertices[strengthVertexIndex] = strengthVertex;
+
+    triangles[0] = 0;
+    triangles[1] = vitallityVertexIndex;
+    triangles[2] = abillityVertexIndex;
+    
+    triangles[3] = 0;
+    triangles[4] = abillityVertexIndex;
+    triangles[5] = charismaVertexIndex;
+    
+    triangles[6] = 0;
+    triangles[7] = charismaVertexIndex;
+    triangles[8] = agillityVertexIndex;
+    
+    triangles[9] = 0;
+    triangles[10] = agillityVertexIndex;
+    triangles[11] = strengthVertexIndex;
+    
+    triangles[12] = 0;
+    triangles[13] = strengthVertexIndex;
+    triangles[14] = vitallityVertexIndex;
+
+    mesh.vertices = vertices;
+    mesh.uv = uv;
+    mesh.triangles = triangles;
+    radarMeshCanvasRenderer.SetMesh(mesh);
+    radarMeshCanvasRenderer.SetMaterial(radarMaterial, null);
   }
+  
 }
