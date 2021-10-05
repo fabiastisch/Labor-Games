8ba7bcb3b17d203574fd7a471e8276f70a90c9f8
Fabian Weber <fabiandotweber@gmail.com>
2021-10-05 06:49:50 +0200

    Add simple test Enemy

---
+++ Assets/Scripts/Combat/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Combat {
+    public class Enemy : MonoBehaviour {
+        public float movementSpeed = 3f;
+        private Transform target;
+        private bool isAtTarget = false;
+        [SerializeField] private float canAttack;
+        private float attackSpeed = 1f;
+        private float attackDamage = 10f;
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                target = other.transform;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                target = null;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                isAtTarget = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                isAtTarget = false;
+            }
+        }
+
+        private void FixedUpdate() {
+            if (target != null) {
+                if (!isAtTarget) {
+                    float step = movementSpeed * Time.deltaTime;
+                    transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+                }
+                else {
+                    if (attackSpeed <= canAttack) {
+                        target.gameObject.GetComponent<Health>().UpdateHealth(-attackDamage);
+                        canAttack = 0f;
+                    }
+                    else canAttack += Time.deltaTime;
+                }
+            }
+        }
+    }
+}
