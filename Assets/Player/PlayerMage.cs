e6ca81b088a2f0a76136c0ff72d3c85be6b68778
Julian Beitinger <49885564+AnimaComedenti@users.noreply.github.com>
2021-10-14 14:30:09 +0200

    More class Scripts
    Added Tank, Mage and DMGDealer Scripts

---
+++ Assets/Player/PlayerMage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMage : PlayerBase
+    {
+        private void Update()
+        {
+            PlayerBaseMovementHandler();
+        }
+
+        private void FixedUpdate()
+        {
+            PlayerBaseStateHandler();
+        }
+        #region Spellcast
+        public override void CastAbillity1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CastAbillity2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CastAbillity3()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CastAbillity4()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CastAbillity5()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
+
