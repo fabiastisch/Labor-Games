aa975091d1716c90edf901f9c91d9db3151b79b8
XxmatrixenxX <Falko116@freenet.de>
2021-12-03 01:48:26 +0100

    Added Stuff

---
+++ Assets/LevelingAndClasses/LevelingScripts/Passives/Spells/SpellScript/FireWall.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Combat;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class FireWall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int baseDamage = 20;
+    public int activeTime = 5;
+    
+
+    public DamageType damageType = DamageType.Magical;
+    //public GameObject impactEffect;
+    
+    void Start()
+    {
+        Invoke("DestroyAfterTime", activeTime ); 
+    }
+    
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Here to do in our Case Dmg
+        if (other.gameObject.layer == 6)
+        {
+            other.GetComponent<Enemy>()?.TakeDamage(baseDamage, damageType);
+            Debug.Log("Hit something with FireWall");
+        }
+        
+        
+        //TODO From Breakys if we have a Enemy we can trigger Dmg on it
+        // Enemy enemy = GitInfo.GetComponent<Enemy>();
+        // if (enemy != null)
+        // {
+        //     enemy.TakeDamage(damage);
+        // }
+        
+        Debug.Log("Hit something with FireWall");
+
+        //TODO if we want impactEffects we can use this
+        //Instantiate(impactEffect, transform.position, transform.rotation);
+        
+        //Destroys the Object (Projectile)
+    }
+
+    void DestroyAfterTime()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Managers/ResourceManager/ResourceTypeSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Manager/ResourceType")]
+public class ResourceTypeSO : ScriptableObject
+{
+    public string nameString;
+}
