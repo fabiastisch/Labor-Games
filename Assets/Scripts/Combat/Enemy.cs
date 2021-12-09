f61d51c96754b123dc6e519aa4ceff8315d67dff
Fabian Weber <fabiastisch@gmail.com>
2021-12-09 06:58:42 +0100

    Cleanup

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -13,6 +13,7 @@ namespace Combat {
         protected float attackDamage = 10f;
 
         protected override void Die() {
+            // TODO: Drop Loot and Exp
             Destroy(gameObject);
         }
     }
