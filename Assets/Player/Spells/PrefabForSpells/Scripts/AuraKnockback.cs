ce642c226c81da657c5c76a298e23a31de6b5bc8
XxmatrixenxX <Falko116@freenet.de>
2021-12-29 13:46:29 +0100

    added aura knockback and eddited bools

--- Assets/Player/Spells/PrefabForSpells/Scripts/AuraKnockback.cs
+++ Assets/Player/Spells/PrefabForSpells/Scripts/AuraKnockback.cs
@@ -1,4 +1,6 @@
-﻿using Combat;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Combat;
 using UnityEngine;
 using Utils;
 
@@ -6,13 +8,45 @@ using Utils;
 public class AuraKnockback : MonoBehaviour
 {
     public DamageType damageType = DamageType.Physical;
-    //public GameObject impactEffect;
+    private List<Collider2D> enemyList = new List<Collider2D>();
     public float force = 0.02f;
+    
+    [SerializeField] 
+    private float timer = 0.25f;
+    [SerializeField]
+    private float timerMax = 0.25f;
 
     /**
      * If Something Enters hitbox it gets dmg
      */
     private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!enemyList.Contains(other))
+        {
+            enemyList.Add(other);
+
+            if (other.gameObject.layer == 6)
+            {
+                Knockback(other);
+            }
+        }
+    }
+    
+    private void Update()
+    {
+        //use time do Subtract things
+        timer -= Time.deltaTime;
+        //after Time is over do something and restart Timer
+        if (timer <= 0f)
+        {
+            KnockBackList();
+            timer += timerMax;
+        }
+        
+    }
+    
+
+    private void Knockback(Collider2D other)
     {
         if (other.gameObject.layer == 6)
         {
@@ -21,6 +55,36 @@ public class AuraKnockback : MonoBehaviour
                 (other.gameObject.transform.position - Util.GetLocalPlayer().gameObject.transform.position)
                 .normalized;
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(enemyList.Contains(other))
+            enemyList.Remove(other);
+    }
+
+    private void KnockBackList()
+    {
+        if (!enemyList.Any())
+        {
+            return;
+        }
+
+        for (int counter = enemyList.Count - 1; counter >= 0; counter--)
+        {
+            if (enemyList[counter] != null)
+            {
+                GameObject other = enemyList[counter].gameObject;
+                if (other.gameObject.layer == 6)
+                {
+                    Debug.LogWarning("Hit The Bat With the FUCKING Head");
+                    Vector2 direction =
+                        (other.transform.position - Util.GetLocalPlayer().gameObject.transform.position)
+                        .normalized;
+                    other.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+                }
+            }
         }
     }
 
