0699a0a75a2804f683ad1bbd798e99360292952e
XxmatrixenxX <Falko116@freenet.de>
2021-12-29 04:57:52 +0100

    added bools to statTypes

--- Assets/Scripts/Managers/StatScript/StatTypeSO.cs
+++ Assets/Scripts/Managers/StatScript/StatTypeSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,4 +7,32 @@ using UnityEngine;
 public class StatTypeSO : ScriptableObject
 {
     public string nameString;
+
+    public bool hasBoolValue = false;
+
+    public bool boolValue = false;
+
+    private void Awake()
+    {
+        if (boolValue == true)
+        {
+            hasBoolValue = true;
+        }
+    }
+
+    public void BoolIsTrue()
+    {
+        if (hasBoolValue == true)
+        {
+            boolValue = true;
+        }
+    }
+    
+    public void BoolIsFalse()
+    {
+        if (hasBoolValue == true)
+        {
+            boolValue = false;
+        }
+    }
 }
