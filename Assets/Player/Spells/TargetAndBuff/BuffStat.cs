abde99c24d39f49e631c16eaba00557ec9cd5ffe
XxmatrixenxX <Falko116@freenet.de>
2022-01-15 17:31:59 +0100

    added more spells

--- Assets/Player/Spells/TargetAndBuff/BuffStat.cs
+++ Assets/Player/Spells/TargetAndBuff/BuffStat.cs
@@ -1,20 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Player.Spells.TargetAndBuff
 {
+    [CreateAssetMenu(menuName = "ScriptableObject/Spell/Buffstat")]
     public class BuffStat: BuffMaker
     {
-        [SerializeField] private StatType typeToBuff;
-        [SerializeField] private float valueOfBuff;
-        
+        [Header("List of Buffs")] [SerializeField]
+        private List<BuffValue> buffList = new List<BuffValue>();
+
         public override void Buff()
         {
-            StatManager.Instance.AddStat(StatManager.Instance.statTypeList.list[(int) typeToBuff], valueOfBuff);
+            foreach (var VARIABLE in buffList)
+            {
+                StatManager.Instance.AddStat(StatManager.Instance.statTypeList.list[(int) VARIABLE.typeToBuff], VARIABLE.valueOfBuff);
+            }
         }
 
         public override void Debuff()
         {
-            StatManager.Instance.RemoveStat(StatManager.Instance.statTypeList.list[(int) typeToBuff], valueOfBuff);
+            foreach (var VARIABLE in buffList)
+            {
+                StatManager.Instance.RemoveStat(StatManager.Instance.statTypeList.list[(int) VARIABLE.typeToBuff], VARIABLE.valueOfBuff);
+            }
+        }
+        
+        [System.Serializable]
+        public class BuffValue
+        {
+            [SerializeField] public StatType typeToBuff;
+            [SerializeField] public float valueOfBuff;
         }
     }
+
+    
 }
