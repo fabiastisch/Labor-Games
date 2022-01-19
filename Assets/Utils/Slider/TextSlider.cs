24808e7311955afc74617780a32e08dfd2de60b4
Fabian Weber <Fabiastisch@gmail.com>
2022-01-19 11:12:41 +0100

    Adjust Slider and Bottom UI

--- Assets/Utils/Slider/TextSlider.cs
+++ Assets/Utils/Slider/TextSlider.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +9,15 @@ public class TextSlider : MonoBehaviour
     [SerializeField] protected TMP_Text _text;
     [SerializeField] protected Slider _slider;
 
+    [SerializeField] protected Color flashColor = Color.white;
+    private float flashTime = 0.08f;
+
+    private Color primaryColor;
+    protected virtual void Awake()
+    {
+        primaryColor = _slider.fillRect.GetComponent<Image>().color;
+
+    }
     public void SetValue(float value)
     {
         if (float.IsInfinity(value) || float.IsNaN(value))
@@ -19,9 +30,30 @@ public class TextSlider : MonoBehaviour
         }
     }
 
+    public void Flash()
+    {
+        StopCoroutine(nameof(FlashCoroutine));
+        ResetFlash();
+        StartCoroutine(nameof(FlashCoroutine));
+    }
+
+
     public void SetText(string text)
     {
         _text.text = text;
     }
 
+    IEnumerator FlashCoroutine()
+    {
+        Image image = _slider.fillRect.GetComponent<Image>();
+        image.color = flashColor;
+        yield return new WaitForSeconds(flashTime);
+        image.color = primaryColor;
+    }
+
+    private void ResetFlash()
+    {
+        Image image = _slider.fillRect.GetComponent<Image>();
+        image.color = primaryColor;
+    }
 }
